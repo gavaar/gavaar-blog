193b0d725c6f177847fd9b394f139e9ee268512b
Moi <oxspit@gmail.com>
2024-06-13 22:58:23 +0200

    0.0.26 - views are counted by device instead of by new tab - fix images overflowing width if they are too big

--- src/app/services/views.service.ts
+++ src/app/services/views.service.ts
@@ -12,7 +12,23 @@ export class ViewsService {
   private admin = inject(PermissionsService).admin;
 
   increaseViews(path: string, id: string): Observable<number> {
-    const incrementQuery = (this.admin() || !environment.production) ? of() : updateFbDocument(path, { [id]: increment(1) });
-    return incrementQuery.pipe(concatMap(() => readFbDocument<Views>(path)), map(posts => posts[id]));
+    return this.updateOrIgnoreIncrement(path, id)
+      .pipe(
+        concatMap(() => readFbDocument<Views>(path)),
+        map(posts => posts[id])
+      );
+  }
+
+  private updateOrIgnoreIncrement(path: string, id: string): Observable<void> {
+    const pathKey = `${path}/${id}`;
+    const deviceViewed = JSON.parse(localStorage.getItem('viewed') || '{}');
+    const forbidUpdate = this.admin() || !environment.production; // admins and devs won't update.
+
+    if (!deviceViewed[pathKey] || !forbidUpdate) {
+      localStorage.setItem('viewed', JSON.stringify({ ...deviceViewed, [pathKey]: true }));
+      return updateFbDocument(path, { [id]: increment(1) });
+    }
+
+    return of(undefined);
   }
 }
